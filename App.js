6ab74769b00cbb8e4e9deb013c5d45a33074a4ce
igBFranco <igor.franco@aluno.unc.br>
2021-12-13 19:40:38 -0300

    Adicionando Dados na Tela Sobre

--- App.js
+++ App.js
@@ -13,11 +13,11 @@ LogBox.ignoreAllLogs(true);
 function HomeScreen({ navigation }) {
   return (
     <View style={{ flex: 1, padding:10}}>
-      <ScrollView contentContainerStyle={{padding:20}} style={styles.container}>
-          <View style={{flexDirection:'row', alignItems:'center'}}>
+          <View style={styles.containerHome}>
             <Image source={{uri: 'http://1.gravatar.com/avatar/1f1c7c76fc6e11ae275fe52525522795?s=200'}} style={{width:100, height:100, borderRadius:100}}/>
             <Text style={styles.textHeader}>Igor Bueno Franco</Text>
           </View>
+      <ScrollView bounces='false' contentContainerStyle={{padding:20}} style={styles.container}>
           <View>
             <TouchableOpacity onPress={()=>navigation.navigate('Sobre')} style={styles.btnNavigation}>
               <Ionicons name="ios-information-circle" size={29} color='white'></Ionicons>
@@ -46,11 +46,63 @@ function HomeScreen({ navigation }) {
 }
 
 function SobreScreen({ navigation }) {
+
+  const [showModal, setModal] = useState(false);
+
+  const abrirModalContato = ()=>{
+    setModal(!showModal);
+  }
+
+  
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Sobre</Text>
-      
-    </View>
+      <View style={{ flex: 1, padding:10}}>
+        <View style={styles.containerSobre}>
+              <Image source={{uri: 'http://1.gravatar.com/avatar/1f1c7c76fc6e11ae275fe52525522795?s=200'}} style={{width:100, height:100, borderRadius:100}}/>
+              <Text style={styles.textHeader}>Igor Bueno Franco</Text>
+        </View>
+        <ScrollView bounces='true' contentContainerStyle={{backgroundColor:'#dedede'}} style={styles.container}>
+            <View style={styles.card}>
+              <View style={styles.cardHeader}>
+                <Ionicons name="ios-information-circle" size={29} color='white'></Ionicons>
+                <Text style={styles.cardHeaderText}>Dados</Text>
+              </View>
+              <Text style={styles.cardText}>21 Anos, Brasileiro e Luxemburguês</Text>
+              <Text style={styles.cardText}>Itaiópolis - Santa Catarina</Text>
+            </View>
+            <View style={styles.card}>
+              <View style={styles.cardHeader}>
+                <Ionicons name="ios-school" size={29} color='white'></Ionicons>
+                <Text style={styles.cardHeaderText}>Formação</Text>
+              </View>
+              <Text style={styles.cardText}>Universidade do Contestado - UnC</Text>
+              <Text style={styles.cardText}>Engenharia de Software</Text>
+              <Text style={styles.cardText}>Bacharelado</Text>
+              <Text style={styles.cardText}>2020 - 2023</Text>
+            </View>
+            <View style={styles.card}>
+              <View style={styles.cardHeader}>
+                <Ionicons name="desktop" size={29} color='white'></Ionicons>
+                <Text style={styles.cardHeaderText}>Tecnologias</Text>
+              </View>
+              <View style={{flexDirection:'row'}}>
+                <Ionicons name="logo-html5" size={29} color='black'></Ionicons>
+                <Text style={styles.cardText}>HTML 5</Text>
+              </View>
+              <View style={{flexDirection:'row'}}>
+                <Ionicons name="logo-css3" size={29} color='black'></Ionicons>
+                <Text style={styles.cardText}>CSS 3</Text>
+              </View>
+              <View style={{flexDirection:'row', alignItems:'center'}}>
+                <Ionicons name="logo-react" size={29} color='black'></Ionicons>
+                <Text style={styles.cardText}>React Native</Text>
+              </View>
+              <TouchableOpacity onPress={() =>abrirModalContato()} style={styles.cardHeaderContact}>
+                <Ionicons name="ios-mail" size={29} color='white'></Ionicons>
+                <Text style={styles.cardHeaderTextContact}>Entre em Contato </Text>
+              </TouchableOpacity>
+            </View>
+        </ScrollView>
+      </View>
   );
 }
 
@@ -165,6 +217,22 @@ const styles = StyleSheet.create({
     backgroundColor:'white',
     borderRadius:20 
   },
+  containerHome:{
+    backgroundColor:'white',
+    borderRadius:20,
+    flexDirection:'row', 
+    alignItems:'center', 
+    padding:20,
+    marginBottom:15
+  },
+  containerSobre:{
+    backgroundColor:'white',
+    borderRadius:20,
+    flexDirection:'row', 
+    alignItems:'center', 
+    padding:20,
+    marginBottom:15
+  },
   textHeader:{
     color:'#1d2e8c',
     fontSize:25,
@@ -185,6 +253,50 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     borderRadius:20,
     justifyContent:'center'
-  }
+  },
+  card:{
+    backgroundColor:'#dedede',
+    flex:1,
+    height:'100%',
+    borderRadius:20,
+  },
+  cardHeader:{
+    backgroundColor:'#1d2e8c',
+    padding:15,
+    borderRadius:20,
+    //shadowColor:'black',
+    //shadowOpacity:50,
+    flexDirection:'row',
+    alignItems:'center',
+    height:80,
+    marginBottom:10
+  },
+  cardHeaderText:{
+    fontSize:25,
+    color:'white',
+    padding:5,
+  },
+  cardText:{
+    fontSize:15,
+    color:'black',
+    padding:20,
+    alignItems:'center'
+  },
+  cardHeaderContact:{
+    backgroundColor:'#fca503',
+    padding:15,
+    borderRadius:20,
+    //shadowColor:'black',
+    //shadowOpacity:50,
+    flexDirection:'row',
+    alignItems:'center',
+    height:80,
+    marginTop:10
+  },
+  cardHeaderTextContact:{
+    fontSize:25,
+    color:'white',
+    padding:5,
+  },
 
 });
